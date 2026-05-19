5536f18748b61625b58a0a23dbe0a4d84a22df57
armin-akhtarzand <armin.akhtarzand@outlook.com>
2026-05-19 13:05:45 +0200

    add gh actions

---
+++ src/main/java/se/iths/armin/webshoporderservice/WebshopOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package se.iths.armin.webshoporderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopOrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/armin/webshoporderservice/WebshopOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.armin.webshoporderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
